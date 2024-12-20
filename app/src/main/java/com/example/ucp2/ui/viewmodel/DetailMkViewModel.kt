f703bdd06b3fb3ab9dc890b86060bd32144ea663
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 01:10:14 +0700

    Membuat class DetailMkViewModel di mana memiliki fungsi deletmk

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailMkViewModel.kt
@@ -1,7 +1,64 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
+class DetailMkViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMk: RepositoryMk,
+) : ViewModel(){
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMk.getMk(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    erorMessage = it.message ?: "Terjadi Kesalahan",
+
+                    )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMk(){
+        detailUiState.value.detailUiEvent.toMataKuliahEntity().let {
+            viewModelScope.launch {
+                repositoryMk.deleteMk(it)
+            }
+        }
+    }
+}
 
 data class DetailUiState(
     val detailUiEvent :MataKuliahEvent = MataKuliahEvent(),
