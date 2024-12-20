5c16f6a03719cc82cffeac8c7a90a4f98582f2ea
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 00:21:32 +0700

    Memebuat data class

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeMkViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.MataKuliah
+
+
+data class HomeMkUiState(
+    val listMhs: List<MataKuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String? = ""
+)
