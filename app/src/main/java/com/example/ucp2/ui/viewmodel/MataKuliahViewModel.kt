fdebf36748b894c08389008f10e6c24871386b9d
wawanpi <muhaguskurniawan@gmail.com>
2024-12-20 23:10:55 +0700

    menambahkan data class MataKuliahEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -2,6 +2,22 @@ package com.example.ucp2.ui.viewmodel
 
 import com.example.ucp2.data.entity.MataKuliah
 
+
+
+data class MkFormErrorState(
+    val kode: String? =null,
+    val nama: String? =null,
+    val sks: String? =null,
+    val semester: String? =null,
+    val jenis: String? =null,
+    val dosenPengampu: String?=null,
+){
+    fun isValid(): Boolean {
+        return kode == null && nama == null && sks == null &&
+                semester == null && jenis == null && dosenPengampu == null
+    }
+}
+
 data class MataKuliahEvent(
     val kode: String = "",
     val nama: String = "",
