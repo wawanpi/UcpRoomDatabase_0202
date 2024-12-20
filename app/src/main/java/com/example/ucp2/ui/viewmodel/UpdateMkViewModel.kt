1b4e2eeb64f05265c4e4320003358da13e32ce71
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 01:23:40 +0700

    Membuat class UpdateMkViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/UpdateMkViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.repository.RepositoryMk
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMkViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMk: RepositoryMk
+) : ViewModel() {
+    var updateUiState by mutableStateOf(MkUiState())
+        private set
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiUpdate.KODE])
+
+    init {
+        viewModelScope.launch{
+            updateUiState = repositoryMk.getMk(_kode)
+                .filterNotNull()
+                .first()
+                .toUIStateMk()
+        }
+    }
+
+    fun updateState(mataKuliahEvent: MataKuliahEvent) {
+        updateUiState = updateUiState.copy(
+            mataKuliahEvent = mataKuliahEvent,
+        )
+    }
+    fun validateFields(): Boolean {
+        val event = updateUiState.mataKuliahEvent
+        val errorState = MkFormErrorState(
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "Sks tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            dosenPengampu = if (event.dosenPengampu.isNotEmpty()) null else "Dosen Pengampu tidak boleh kosong"
+        )
+        updateUiState = updateUiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
