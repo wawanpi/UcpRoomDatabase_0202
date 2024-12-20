29f4570e95807ab1e04a8c1554aef2b1dcdc6e58
wawanpi <muhaguskurniawan@gmail.com>
2024-12-20 20:45:33 +0700

    Membuat class HomeDsnView

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDsnView.kt
@@ -1,7 +1,51 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDsn
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeDsnView(
+
+    private val repositoryDsn: RepositoryDsn
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryDsn.getAllDsn()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listDsn = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 data class HomeUiState(
     val listDsn: List<Dosen> = listOf(),
     val isLoading: Boolean = false,
