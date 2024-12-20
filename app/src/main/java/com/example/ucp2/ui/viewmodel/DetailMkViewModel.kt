69d5db274d7d217e760c7bc3b1e6b215fcd2192a
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 01:04:49 +0700

    Membuat data class DetailUiState

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailMkViewModel.kt
@@ -2,6 +2,24 @@ package com.example.ucp2.ui.viewmodel
 
 import com.example.ucp2.data.entity.MataKuliah
 
+
+data class DetailUiState(
+    val detailUiEvent :MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val erorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
+
+    val isEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
+
+}
+
+/* Data class untuk menampung data yang akan ditampilkan di UI*/
+
+//memindahkan data dari entity ke ui
 fun MataKuliah.toDetailUiEvent(): MataKuliahEvent{
     return MataKuliahEvent(
         kode = kode,
