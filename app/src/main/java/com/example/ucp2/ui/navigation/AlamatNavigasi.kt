a96afae331c5a6afc0a528dab19fb755126e45df
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 01:31:35 +0700

    Membuat object DestinasiHome detail dan update

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,20 @@
+package com.example.ucp2.ui.navigation
+
+import com.example.ucp2.ui.navigation.DestinasiDetail.KODE
+
+interface AlamatNavigasi {
+    val route: String
+}
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val KODE = "kode"
+    val routeWithArgs = "$route/{$KODE}"
+}
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val kode = "kode"
+    val routeWithArg = "$route/{$KODE}"
+}
