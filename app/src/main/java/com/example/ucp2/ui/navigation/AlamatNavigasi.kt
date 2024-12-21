58cf50fa1ee36ace9f535ebfe4523b89649cce6d
wawanpi <muhaguskurniawan@gmail.com>
2024-12-22 00:18:16 +0700

    Menambahkan object HomeDsnRoute dan HomeMkRoute

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -2,17 +2,29 @@ package com.example.ucp2.ui.navigation
 
 import com.example.ucp2.ui.navigation.DestinasiDetail.KODE
 
+
 interface AlamatNavigasi {
     val route: String
 }
-object DestinasiHome : AlamatNavigasi {
+
+object HomeRoute : AlamatNavigasi {
     override val route = "home"
 }
+
+object HomeDsnRoute : AlamatNavigasi {
+    override val route = "home_dsn"
+}
+
+object HomeMkRoute : AlamatNavigasi {
+    override val route = "home_mk"
+}
+
 object DestinasiDetail : AlamatNavigasi {
     override val route = "detail"
     const val KODE = "kode"
     val routeWithArgs = "$route/{$KODE}"
 }
+
 object DestinasiUpdate : AlamatNavigasi {
     override val route = "update"
     const val KODE = "kode"
