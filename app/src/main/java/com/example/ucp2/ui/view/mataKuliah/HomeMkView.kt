3344f89f3b50c79c2d57488f9115666842f20d78
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 08:26:35 +0700

    Membuat Fungsi HomeMkView

--- app/src/main/java/com/example/ucp2/ui/view/mataKuliah/HomeMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/mataKuliah/HomeMkView.kt
@@ -10,17 +10,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -28,9 +34,54 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.ui.costumwidget.CustomTopAppBar
 import com.example.ucp2.ui.viewmodel.HomeMkUiState
+import com.example.ucp2.ui.viewmodel.HomeMkViewModel
+import com.example.ucp2.ui.viewmodel.PenyediaMkViewModel
 import kotlinx.coroutines.launch
+
+@Composable
+fun HomeMkView(
+    viewModel : HomeMkViewModel = viewModel(factory = PenyediaMkViewModel.Factory),
+    onAddMk: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier,
+){
+    Scaffold (
+        topBar = {
+            CustomTopAppBar(
+                judul = "Daftar Mata Kuliah",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMk,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Matakuliah",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeMkUiState by viewModel.homeMkUiState.collectAsState()
+
+        BodyHomeMkView(
+            homeMkUiState = homeMkUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 fun BodyHomeMkView(
