d238caaf07b3bfb01a98624007fcb57c7fa8748f
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 08:00:00 +0700

    Membuat Fungsi ListMataKuliah

--- app/src/main/java/com/example/ucp2/ui/view/mataKuliah/HomeMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/mataKuliah/HomeMkView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -21,6 +23,28 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.MataKuliah
 
+@Composable
+fun ListMataKuliah(
+    listMk: List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listMk,
+            itemContent = { mk ->
+                CardMk(
+                    mk = mk,
+                    onClick = {
+                        onClick(mk.kode)
+                    }
+                )
+            }
+        )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
