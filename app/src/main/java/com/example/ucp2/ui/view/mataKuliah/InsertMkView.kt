62f090de0f46ca05e942bb792a1afb684b9d1622
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 09:00:19 +0700

    Membuat Fungsi InsertBodyMk

--- app/src/main/java/com/example/ucp2/ui/view/mataKuliah/InsertMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/mataKuliah/InsertMkView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -18,7 +19,36 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.ucp2.ui.viewmodel.MataKuliahEvent
 import com.example.ucp2.ui.viewmodel.MkFormErrorState
+import com.example.ucp2.ui.viewmodel.MkUiState
 
+
+@Composable
+fun InsertBodyMk(
+    modifier: Modifier = Modifier,
+    onValueChange: (MataKuliahEvent) -> Unit,
+    uiState: MkUiState,
+    onClick: () -> Unit
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMataKuliah(
+            mataKuliahEvent = uiState.mataKuliahEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
 
 @Composable
 fun FormMataKuliah(
