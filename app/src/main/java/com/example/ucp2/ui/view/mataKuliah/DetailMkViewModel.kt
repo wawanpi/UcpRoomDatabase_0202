2e7e3f937691ff3478437d4db399d6dce5443055
wawanpi <muhaguskurniawan@gmail.com>
2024-12-21 09:25:26 +0700

    Membuat Fungsi privet DeleteConfirmationDialog

---
+++ app/src/main/java/com/example/ucp2/ui/view/mataKuliah/DetailMkViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.ucp2.ui.view.mataKuliah
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCacel: () -> Unit, modifier: Modifier =
+        Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do Nothing */},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCacel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
